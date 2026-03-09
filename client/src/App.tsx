abb8ff44bb02a8d292655bc8ca0176cc38304b27
Prisantha <pmoodley19@aiip.alueducation.com>
2026-03-09 20:19:05 +0200

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,35 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Services from "@/pages/Services";
+import Pricing from "@/pages/Pricing";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home}/>
+      <Route path="/services" component={Services}/>
+      <Route path="/pricing" component={Pricing}/>
+      <Route path="/about" component={About}/>
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
